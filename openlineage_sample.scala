710f4a3da87307c94d54e965aaf09f708c9a8c29
xuxumin <xu_xumin@outlook.com>
2022-02-03 19:23:57 -0800

    update file path

--- openlineage_sample.scala
+++ openlineage_sample.scala
@@ -3,23 +3,14 @@ import org.apache.spark.sql.types.{StructType, StructField, IntegerType, StringT
 
 // COMMAND ----------
 
-spark.conf.get("spark.openlineage.url.param.code")
-
-// COMMAND ----------
-
 val storageServiceName = spark.conf.get("spark.openlineage.samplestorageaccount")
 val storageContainerName = spark.conf.get("spark.openlineage.samplestoragecontainer")
-val abfssRootPath = "abfss://"+storageContainerName+"@"+storageServiceName+".blob.core.windows.net"
-val abfssOutputPath = "abfss://"+storageContainerName+"@"+storageServiceName+".dfs.core.windows.net"
+val adlsRootPath = "wasbs://"+storageContainerName+"@"+storageServiceName+".blob.core.windows.net"
 
 val storageKey = dbutils.secrets.get("purview-to-adb-kv", "storageAccessKey")
 
 spark.conf.set("fs.azure.account.key."+storageServiceName+".blob.core.windows.net", storageKey)
-spark.conf.set("fs.azure.account.key."+storageServiceName+".dfs.core.windows.net", storageKey)
 
-// COMMAND ----------
-
-abfssRootPath
 
 // COMMAND ----------
 
@@ -32,7 +23,7 @@ val exampleA = (
     spark.read.format("csv")
   .schema(exampleASchema)
   .option("header", true)
-  .load(abfssRootPath+"/examples/data/csv/exampleInputA/exampleInputA.csv")
+  .load(adlsRootPath+"/examples/data/csv/exampleInputA/exampleInputA.csv")
 )
 
 
@@ -45,15 +36,11 @@ val exampleB = (
     spark.read.format("csv")
   .schema(exampleBSchema)
   .option("header", true)
-  .load(abfssRootPath+"/examples/data/csv/exampleInputB/exampleInputB.csv")
+  .load(adlsRootPath+"/examples/data/csv/exampleInputB/exampleInputB.csv")
 )
 
 // COMMAND ----------
 
 val outputDf = exampleA.join(exampleB, exampleA("id") === exampleB("id"), "inner").drop(exampleB("id"))
 
-outputDf.repartition(1).write.mode("overwrite").format("csv").save("dbfs:/temp/examples/data/csv/exampleOutput/")
-
-// COMMAND ----------
-
-
+outputDf.repartition(1).write.mode("overwrite").format("csv").save(adlsRootPath+"/examples/data/csv/exampleOutput/")
